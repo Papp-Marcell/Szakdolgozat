960a378e1c10478236b09e65892b5bad68cba222
Papp Marcell <pmarci725@gmail.com>
2022-11-08 20:54:43 +0100

    Added Sources to latex

--- Szakdolgozat/Program.cs
+++ Szakdolgozat/Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+//Singleton regisztrálása
 builder.Services.AddSingleton<MyFile>();
 builder.Services.AddSingleton<MockInstructionList>();
 builder.Services.AddSingleton<MenuService>();
